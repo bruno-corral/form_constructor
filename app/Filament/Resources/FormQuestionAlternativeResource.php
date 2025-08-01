f68056f94af439a064f160c521c205fb1e10c7f4
bruno <brunoalcorral@gmail.com>
2025-08-01 12:44:56 -0300

    feat: Remover código comentado
    - Removido codigo comentado em FormQuestionAlternativeResource

--- app/Filament/Resources/FormQuestionAlternativeResource.php
+++ app/Filament/Resources/FormQuestionAlternativeResource.php
@@ -3,17 +3,9 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\FormQuestionAlternativeResource\Pages;
-use App\Filament\Resources\FormQuestionAlternativeResource\RelationManagers;
-use App\Models\Form as ModelsForm;
 use App\Models\FormQuestionAlternative;
-use App\Models\Form as FormModel;
-use App\Models\Question;
-use Filament\Forms\Components\Card;
-use Filament\Forms\Components\CheckboxList;
 use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Components\Hidden;
-use Filament\Forms\Components\Placeholder;
-use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Form;
 use Filament\Infolists\Components\TextEntry;
@@ -23,8 +15,6 @@ use Filament\Tables;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 
 class FormQuestionAlternativeResource extends Resource
@@ -41,7 +31,6 @@ class FormQuestionAlternativeResource extends Resource
     {
         return $form
             ->schema([
-                // Meu codigo
                 Fieldset::make('Content')
                     ->schema([
                         Hidden::make('user_id')
@@ -76,90 +65,6 @@ class FormQuestionAlternativeResource extends Resource
                             ->label('Alternative'),
                     ])
                     ->columns(3),
-
-
-                // Meu novo codigo
-                // Fieldset::make('Content')
-                //     ->schema([
-                //         Select::make('form_id')
-                //             ->relationship(
-                //                 'form', 
-                //                 'title', 
-                //                 fn ($query) => $query->where('is_active', true)
-                //             )
-                //             ->required()
-                //             ->placeholder('Choose a form')
-                //             ->label('Form')
-                //             ->reactive()
-                //             ->afterStateUpdated(fn ($state, callable $set) => $set('questions', FormModel::with('questions')->where('form_id', $state)->get())),
-                //             // ->afterStateUpdated(fn ($state, callable $set) => dd($state)),
-
-                //         Placeholder::make('question_id')
-                //             ->content(function (callable $get) {
-                //                 dd($get);
-                //                 $question = $get('form_id');
-
-                //                 // dd($question);
-                                
-                //                 if (!$question) {
-                //                     return 'Selecione um formulário para carregar as perguntas.';
-                //                 }
-
-                //                 $output = '';
-                //                 foreach ($questions as $index => $question) {
-                //                     $output .= "<strong>" . ($index + 1) . ". {$question->title}</strong><br>";
-                //                     // foreach ($question->alternatives as $altIndex => $alternativa) {
-                //                     //     $letra = chr(65 + $altIndex); // A, B, C...
-                //                     //     $output .= "{$letra}. {$alternativa->texto}<br>";
-                //                     // }
-                //                     $output .= "<br>";
-                //                 }
-                //                 return $output;
-                //             }),
-                //         // Select::make('question_id')
-                //         //     ->relationship('question', 'title')
-                //         //     ->required()
-                //         //     ->placeholder('Choose a question')
-                //         //     ->label('Question'),
-                //         // Select::make('alternative_id')
-                //         //     ->relationship('alternative', 'title')
-                //         //     ->required()
-                //         //     ->placeholder('Choose a alternative')
-                //         //     ->label('Alternative'),
-                //     ])
-                //     ->columns(3),
-
-
-
-                // Card::make([
-                //     Select::make('form_id')
-                //         ->label('Formulário')
-                //         ->options(Formulario::all()->pluck('titulo', 'id'))
-                //         ->reactive()
-                //         ->afterStateUpdated(fn ($state, callable $set) => $set('perguntas', Pergunta::with('alternatives')->where('form_id', $state)->get())),
-                    
-                //     Placeholder::make('perguntas_placeholder')
-                //         ->content(function (callable $get) {
-                //             $perguntas = $get('perguntas');
-                            
-                //             if (! $perguntas) {
-                //                 return 'Selecione um formulário para carregar as perguntas.';
-                //             }
-
-                //             $output = '';
-                //             foreach ($perguntas as $index => $pergunta) {
-                //                 $output .= "<strong>" . ($index + 1) . ". {$pergunta->texto}</strong><br>";
-                //                 foreach ($pergunta->alternativas as $altIndex => $alternativa) {
-                //                     $letra = chr(65 + $altIndex); // A, B, C...
-                //                     $output .= "{$letra}. {$alternativa->texto}<br>";
-                //                 }
-                //                 $output .= "<br>";
-                //             }
-                //             return $output;
-                //         })
-                //         ->columnSpanFull()
-                //         ->hidden(fn (callable $get) => empty($get('perguntas'))),
-                // ]),
             ]);
     }
 
